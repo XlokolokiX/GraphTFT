1fe3481293e39d20753c6569ef53e06497aa830a
XlokolokiX <alfonsomouton@gmail.com>
2024-10-14 23:13:59 -0300

    modified Format

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ TFT_eSPI tft = TFT_eSPI();
 uint16_t x[N];
 uint16_t y[N];
 
-Graph_TFT canva = Graph_TFT(&tft, 4, 4, 124, 120, 15, 5, PAPER);
+Graph_TFT canva = Graph_TFT(&tft, 4, 4, 124, 100, 15, 5, NEON);
 
 void setup() 
 {
@@ -31,16 +31,16 @@ void setup()
     x[i] = i;
   }
 
-  uint8_t percentage[5] = {12,28,10,10,40};
-  const char* labels[5] = {"Aa", "Bb", "Cc", "Dd", "Ee"};
-  canva.setDataPIE(percentage, 5, labels);
+  //uint8_t percentage[5] = {12,28,10,10,40};
+  //const char* labels[5] = {"Aa", "Bb", "Cc", "Dd", "Ee"};
+  //canva.setDataPIE(percentage, 5, labels);
 }
 
 void loop() 
 {
-  // delay(1000);
-  // for(int i = 0; i<N ; i++){
-  //   y[i] = rand() % 10;
-  // }
-  // canva.setDataLINES(x, y, N, 10);
+  delay(1000);
+  for(int i = 0; i<N ; i++){
+    y[i] = rand() % 10;
+  }
+  canva.setDataLINES(x, y, N, 10);
 }
